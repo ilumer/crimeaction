42bf77f639fe0339c77255962f823b3a9d4691b9
ilumer <18888106831qxq@gmail.com>
2016-02-19 20:24:43 +0800

    add viewpager

---
+++ app/src/main/java/com/example/qiao/crimeaction/CrimePagerActivity.java
@@ -0,0 +1,69 @@
+package com.example.qiao.crimeaction;
+
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.support.v13.app.FragmentStatePagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+import java.util.ArrayList;
+import java.util.UUID;
+
+public class CrimePagerActivity extends AppCompatActivity {
+    private ViewPager mViewPager;
+    private ArrayList<crime> mCrimes;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mViewPager = new ViewPager(this);
+        mViewPager.setId(R.id.viewPager);
+        setContentView(mViewPager);
+
+        mCrimes = crimelab.getScrimelab(this).getListCrimes();
+
+        FragmentManager fragmentManager = getFragmentManager();
+
+        mViewPager.setAdapter(new FragmentStatePagerAdapter(fragmentManager) {
+            @Override
+            public Fragment getItem(int position) {
+                crime mcrime = mCrimes.get(position);
+                return CrimeFragment.newInstance(mcrime.getmId());
+            }
+
+            @Override
+            public int getCount() {
+                return mCrimes.size();
+            }
+        });
+
+        //setOnpagechangedlistener 应该去除了
+        mViewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+            }
+            @Override
+            public void onPageSelected(int position) {
+                crime c = mCrimes.get(position);
+                Log.e("hello",c.getmTitle());
+                if (c.getmTitle()!=null)
+                    setTitle(c.getmTitle());
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+
+            }
+        });
+        UUID uuid = (UUID)getIntent().getSerializableExtra(CrimeFragment.ExTRA_CRIME_ID);
+        for (int i =0;i<mCrimes.size();i++){
+            if (mCrimes.get(i).getmId()==uuid){
+                mViewPager.setCurrentItem(i);
+                break;
+            }
+        }
+    }
+}
