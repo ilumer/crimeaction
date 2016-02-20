d39f570351d2df647e9f9b393c85cdaba7a9c0b5
ilumer <18888106831qxq@gmail.com>
2016-02-20 20:10:38 +0800

    add dialogfragment

--- app/src/main/java/com/example/qiao/crimeaction/CrimeFragment.java
+++ app/src/main/java/com/example/qiao/crimeaction/CrimeFragment.java
@@ -2,11 +2,14 @@ package com.example.qiao.crimeaction;
 
 import android.app.Activity;
 import android.app.Fragment;
+import android.app.FragmentManager;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -17,16 +20,21 @@ import android.widget.EditText;
 
 
 import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.Locale;
 import java.util.UUID;
 
-public class CrimeFragment extends android.app.Fragment{
+public class CrimeFragment extends Fragment{
     private crime mCrime;
     private EditText mTitleFiled;
     private Button mDateButton;
     private CheckBox mSolvedCheckedBox;
-    public static String ExTRA_CRIME_ID =
+    public static final String ExTRA_CRIME_ID =
             "com.example.qiao.crime_id";
+
+    public static final int RESULT_DATE = 0;
+
+    public static String DIALOG_DATE = "date";
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -44,8 +52,17 @@ public class CrimeFragment extends android.app.Fragment{
         //false 是否将生成的视图传递给父视图
         mTitleFiled = (EditText) view.findViewById(R.id.Crime_title);
         mDateButton = (Button) view.findViewById(R.id.showTime);
-        mDateButton.setText(GetSuitableDateformat());
-        mDateButton.setEnabled(false);
+        //mDateButton.setText(GetSuitableDateformat());
+        upDate();
+        mDateButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FragmentManager fragmentManager = getFragmentManager();
+                DatePickerFragment datePickerFragment = DatePickerFragment.newInstance(mCrime.getmDate());
+                datePickerFragment.setTargetFragment(CrimeFragment.this,RESULT_DATE);
+                datePickerFragment.show(fragmentManager,DIALOG_DATE);
+            }
+        });
         mSolvedCheckedBox = (CheckBox) view.findViewById(R.id.Issolved);
         mSolvedCheckedBox.setChecked(mCrime.getmSloved());
         mSolvedCheckedBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
@@ -76,9 +93,10 @@ public class CrimeFragment extends android.app.Fragment{
     }
 
     private String GetSuitableDateformat(){
-        String format = "EEEE:MMM:d:yyyy";
-        SimpleDateFormat formats = new SimpleDateFormat(format, Locale.CHINA);
-        return formats.format(mCrime.getmDate());
+        //String format = "EEEE:MMM:d:yyyy";
+        //SimpleDateFormat formats = new SimpleDateFormat(format, Locale.CHINA);
+        //return formats.format(mCrime.getmDate());
+        return mCrime.getmDate().toString();
     }
 
     public static Fragment newInstance(UUID crimeId){
@@ -90,6 +108,28 @@ public class CrimeFragment extends android.app.Fragment{
     }
 
     public void returnResult(){
-        getActivity().setResult(Activity.RESULT_OK,null);
+        getActivity().setResult(Activity.RESULT_OK, null);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+            if (resultCode!=Activity.RESULT_OK)
+                return;
+            if (requestCode==RESULT_DATE){
+                Date date = (Date) data
+                        .getSerializableExtra(DatePickerFragment.EXTRA_DATE);
+
+                mCrime.setmDate(date);
+                if (date==null){
+                    Log.e("hello","hello");
+                }
+                //mDateButton.setText(date.toString());
+                upDate();
+            }
+
+    }
+
+    public void upDate(){
+        mDateButton.setText(mCrime.getmDate().toString());
     }
 }
